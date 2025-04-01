277769eba59a1332bebe3d1f6c0f8e031d9b1dc0
Yina Yoon <yina.yoon@samsung.com>
2025-04-02 08:40:31 +0900

    Refactoring) 1. Fix the bug where the address is greater than or equal to 100. 2. execute() refactoring

--- SSD/SSD_Read.cpp
+++ SSD/SSD_Read.cpp
@@ -5,38 +5,32 @@
 
 class ReadSSD {
 public:
-	void execute(std::map<int, std::string>& nand, int addr) {
-		// ofstream 객체 생성 (파일이 없다면 생성되고, 있으면 열립니다)
-		std::ofstream outFile(filePath, std::ios::trunc);
-
-		// 파일 열기 실패 체크
-		if (!outFile) {
-			std::cout << "파일을 열 수 없습니다!" << std::endl;
+	void execute(const std::map<int, std::string>& nand, int addr) {
+		std::ofstream outputFile(filePath, std::ios::trunc);
+		if (!outputFile.is_open()) {
+			std::cout << "Failed to open file : " << filePath << std::endl;
 			return;
 		}
 
-		if (addr < 0 || addr > 100) {
-			// 잘못 된 주소 일 때 ERROR 기록하기
-			outFile << "ERROR";
-			outFile.close();
+		if (addr < 0 || addr >= 100) {
+			outputFile << "ERROR";
+			outputFile.close();
 			return;
 		}
 
 		auto it = nand.find(addr);
-
-		// 키가 존재하지 않는다면, 0x00000000 쓰기
 		if (it == nand.end()) {
-			outFile << "0x00000000";
-		}
-		else {
-			outFile << it->second;
+			outputFile << VALUE_ZERO;
+			outputFile.close();
+			return;
 		}
 
-		// 파일 닫기
-		outFile.close();
+		outputFile << it->second;
+		outputFile.close();
 		return;
 	}
 
 private:
 	const std::string filePath = "ssd_output.txt";
+	const std::string VALUE_ZERO = "0x00000000";
 };
