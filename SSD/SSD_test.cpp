b49716402be004aeab1553ea55f8aa39d0626b53
yj410.lee <yongjae410@gmail.com>
2025-04-01 14:32:17 +0900

    RED) apply code review comment - change ret param type

--- SSD/SSD_test.cpp
+++ SSD/SSD_test.cpp
@@ -6,19 +6,20 @@ using std::string;
 TEST(SSDTestGroup, ValidReadCommandTest)
 {
 	SSD mySsd;
-	int ret = mySsd.run("R 0");
+	bool ret = mySsd.run("R 0");
+	EXPECT_EQ(ret, true);
 }
 
 TEST(SSDTestGroup, ValidWriteCommandTest)
 {
 	SSD mySsd;
-	int ret = mySsd.run("W 0 0x12345678");
+	bool ret = mySsd.run("W 0 0x12345678");
 	EXPECT_EQ(ret, true);
 }
 
 TEST(SSDTestGroup, InvalidCommandTest)
 {
 	SSD mySsd;
-	int ret = mySsd.run("D 0");
+	bool ret = mySsd.run("D 0");
 	EXPECT_EQ(ret, false);
 }
