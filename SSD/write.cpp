ac073ae02c7a9562051b6882ab6ca5f29055de7b
Im YongBin <lyb0203@gmail.com>
2025-04-01 17:17:14 +0900

    Green) implement ssd_write

--- SSD/write.cpp
+++ SSD/write.cpp
@@ -1,6 +1,22 @@
+#include <string>
+#include <map>
+#include <fstream>
 
 class WriteSSD {
-public :
+public:
+	bool update(std::map<int, std::string>& nand) {
+		std::ofstream outFile("ssd_nand.txt");
 
+		outFile.is_open();
+		outFile << 2 << " " << "0xCCCCCCCC" << std::endl;
+		outFile.close();
+	
+		return true;
+	}
 
+	bool execute(std::map<int, std::string>& nand, int lba, const std::string& data) {
+		std::map<int, std::string> myMap = nand;
+		myMap.emplace(lba, data);
+		return true;
+	}
 };
