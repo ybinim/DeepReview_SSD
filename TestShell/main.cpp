95e8198621690226e51b1b903bd348df563596d6
yj00114.jang <yj00114.jang@samsung.com>
2025-04-02 15:17:00 +0900

    Release 빌드 구현

--- TestShell/main.cpp
+++ TestShell/main.cpp
@@ -1,6 +1,25 @@
 #include "gmock/gmock.h"
+#include "TestShell.h"
+#include "SSDReader.h"
+#include "SSDWriter.h"
 
+#ifdef _DEBUG
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+#else
+int main() {
+	std::string command;
+
+	SSDReader reader;
+	SSDWriter writer;
+	TestShell* shell = new TestShell(&reader, &writer);
+	
+	while (true) {
+		std::cout << "Shell> ";
+		std::getline(std::cin, command);
+		shell->run(command);
+	}
+}
+#endif
