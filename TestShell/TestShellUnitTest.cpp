f00b111907e068d8ee17e9425132a069a70ff34f
Im YongBin <lyb0203@gmail.com>
2025-04-02 14:18:36 +0900

    Red) help command unittest

--- TestShell/TestShellUnitTest.cpp
+++ TestShell/TestShellUnitTest.cpp
@@ -189,3 +189,8 @@ TEST_F(TestShellTestFixture, WrongReadTest)
 	ret = shell->run("read 0 0xAAAAAAAA");
 	EXPECT_EQ(ret, -2);
 }
+
+TEST_F(TestShellTestFixture, HelpCommandTest) {
+	int ret = shell->run("help");
+	EXPECT_EQ(ret, 0);
+}
