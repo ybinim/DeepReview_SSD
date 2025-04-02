fe37c73a28ac2048aeed9d7456f0343bdabce1f4
yj410.lee <yongjae410@gmail.com>
2025-04-02 12:26:05 +0900

    RED)read unit test

--- TestShell/TestShellUnitTest.cpp
+++ TestShell/TestShellUnitTest.cpp
@@ -13,7 +13,13 @@ public:
 	TestShell shell;
 	const string nandTxt = "../SSD/ssd_nand.txt";
 	const string outputTxt = "../SSD/ssd_output.txt";
-	
+
+	void prepareRead() {
+		shell.run("write 0 0xAAAAAAAA");
+		shell.run("write 1 0xBBBBBBBB");
+		shell.run("write 99 0xFFFFFFFF");
+	}
+
 protected:
 	void SetUp() override {
 		remove(nandTxt.c_str());
@@ -26,28 +32,14 @@ protected:
 	}
 };
 
-TEST(TestShellTestGroup, TestShellParamTest)
+TEST_F(TestShellTestFixture, ExitTest)
 {
 	TestShell shell;
-	int ret;
-
-	ret = shell.run("read 3");
-	EXPECT_EQ(ret, 0);
-
-	ret = shell.run("help");
-	EXPECT_EQ(ret, 0);
-
-	ret = shell.run("fullwrite 0xBBBBBBBB");
-	EXPECT_EQ(ret, 0);
-
-	ret = shell.run("fullread");
-	EXPECT_EQ(ret, 0);
-
-	ret = shell.run("exit");
+	int ret = shell.run("exit");
 	EXPECT_EQ(ret, 1);
 }
 
-TEST(TestShellTestGroup, TestShellInvalidCommand)
+TEST_F(TestShellTestFixture, InvalidCommandTest)
 {
 	TestShell shell;
 	int ret;
@@ -64,12 +56,14 @@ TEST_F(TestShellTestFixture, WriteTest)
 	int ret = shell.run("write 3 0xAAAAAAAA");
 	ASSERT_EQ(ret, 0);
 
-	ifstream file(nandTxt.c_str());
+	ifstream file;
+	file.open(nandTxt.c_str());
 	ASSERT_EQ(file.good(), true);
-	
+
 	string data = "";
 	getline(file, data);
 	EXPECT_EQ(data.compare("3 0xAAAAAAAA"), 0);
+	file.close();
 }
 
 TEST_F(TestShellTestFixture, WrongWriteTest)
@@ -87,5 +81,45 @@ TEST_F(TestShellTestFixture, WrongWriteTest)
 	EXPECT_EQ(ret, -2);
 
 	ifstream file(nandTxt.c_str());
+	EXPECT_EQ(file.good(), false);
+}
+
+TEST_F(TestShellTestFixture, ReadTest)
+{
+	int ret = shell.run("read 0");
+	EXPECT_EQ(ret, 0);
+
+	ifstream file;
+	file.open(outputTxt.c_str());
+	ASSERT_EQ(file.good(), true);
+
+	string data = "";
+	getline(file, data);
+	EXPECT_EQ(data.compare("0xAAAAAAAA"), 0);
+	file.close();
+
+	ret = shell.run("read 1");
+	EXPECT_EQ(ret, 0);
+
+	file.open(outputTxt.c_str());
+	ASSERT_EQ(file.good(), true);
+
+	getline(file, data);
+	EXPECT_EQ(data.compare("0xBBBBBBBB"), 0);
+	file.close();
+}
+
+TEST_F(TestShellTestFixture, WrongReadTest)
+{
+	int ret = shell.run("Read 0a");
+	EXPECT_EQ(ret, -2);
+
+	ret = shell.run("Read 100");
+	EXPECT_EQ(ret, -2);
+
+	ret = shell.run("read 0 0xAAAAAAAA");
+	EXPECT_EQ(ret, -2);
+
+	ifstream file(outputTxt.c_str());
 	EXPECT_EQ(file.good(), false);
 }
