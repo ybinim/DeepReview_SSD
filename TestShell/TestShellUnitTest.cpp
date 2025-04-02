fcf4ccf5097aaf3e8de1280372d5f2c398464153
yj410.lee <yongjae410@gmail.com>
2025-04-02 09:00:43 +0900

    RED) add unit test

--- TestShell/TestShellUnitTest.cpp
+++ TestShell/TestShellUnitTest.cpp
@@ -5,6 +5,22 @@ TEST(TestShellTestGroup, TestShellParamTest)
 {
 	TestShell shell;
 	int ret;
+
+	ret = shell.run("write 3 0xAAAAAAAA");
+	EXPECT_EQ(ret, 0);
+
 	ret = shell.run("read 3");
 	EXPECT_EQ(ret, 0);
+
+	ret = shell.run("help");
+	EXPECT_EQ(ret, 0);
+
+	ret = shell.run("fullwrite 0xBBBBBBBB");
+	EXPECT_EQ(ret, 0);
+
+	ret = shell.run("fullread");
+	EXPECT_EQ(ret, 0);
+
+	ret = shell.run("exit");
+	EXPECT_EQ(ret, 1);
 }
