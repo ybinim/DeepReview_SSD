43471606ce7a75d24065d300df4dad7b62a484a2
yj410.lee <yongjae410@gmail.com>
2025-04-03 10:49:37 +0900

    add test runner feature

--- TestShell/main.cpp
+++ TestShell/main.cpp
@@ -2,6 +2,9 @@
 #include "TestShell.h"
 #include "SSDReader.h"
 #include "SSDWriter.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #ifdef _DEBUG
 int main() {
@@ -9,23 +12,44 @@ int main() {
 	return RUN_ALL_TESTS();
 }
 #else
-int main() {
+int main(int argc, char* argv[]) {
 	std::string command;
+	int ret;
 
 	SSDReader reader;
 	SSDWriter writer;
 	TestShell* shell = new TestShell(&reader, &writer);
-	
-	while (true) {
-		std::cout << "Shell> ";
-		std::getline(std::cin, command);
-		int ret = shell->run(command);
 
-		if (ret == 1)
-		{
-			// exit command
-			return 0;
+	if (argc == 1) {
+		while (true) {
+			std::cout << "Shell> ";
+			std::getline(std::cin, command);
+			ret = shell->run(command);
+
+			if (ret == 1)
+			{
+				// exit command
+				return 0;
+			}
 		}
 	}
+	else if (argc == 2) {
+		std::ifstream scriptFile;
+		scriptFile.open(argv[1]);
+		if (scriptFile.is_open()) {
+			while (std::getline(scriptFile, command)) {
+				std::cout << command << "   ___   Run...";
+
+				ret = shell->run(command);
+				if (ret != 0) {
+					break;
+				}
+			}
+			scriptFile.close();
+		}
+		return 0;
+	}
+
+	return -1;
 }
 #endif
