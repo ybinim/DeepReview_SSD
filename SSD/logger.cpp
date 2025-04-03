4aceb55f7c1f98866edad1b4c81cf4f2b86e6cf0
Im YongBin <lyb0203@gmail.com>
2025-04-03 16:08:22 +0900

    log file 10Kb 이상 기록 시 rename

--- SSD/logger.cpp
+++ SSD/logger.cpp
@@ -1,16 +1,61 @@
 #include "logger.h"
 
 ofstream Logger::logFile;
-const string logFileName = "latest.txt";
+const string Logger::logFileName = "latest.log";
+const size_t Logger::MAX_FILE_SIZE = 10240;  // 10 KB
 
-void Logger::print(const string& classFuncName, const string& message) {
+void Logger::openLogFile() {
     if (!logFile.is_open()) {
-        logFile.open(logFileName, std::ios::app);
+        if (fileSizeExceedsLimit(logFileName)) {
+            renameOldLogFile();  
+            logFile.open(logFileName, std::ios::app);
+        }
+        else {
+            logFile.open(logFileName, std::ios::app);
+        }
+
         if (!logFile.is_open()) {
             std::cerr << "Error opening log file!" << std::endl;
-            return;
         }
     }
+}
+
+void Logger::closeLogFile() {
+    if (logFile.is_open()) {
+        logFile.close();
+    }
+}
+
+void Logger::renameOldLogFile() {
+    string newFileName = generateNewFileName();
+    if (std::filesystem::exists(logFileName)) {
+        std::filesystem::rename(logFileName, newFileName);
+    }
+}
+
+string Logger::generateNewFileName() {
+    time_t now = time(nullptr);
+    struct tm timeInfo;
+    localtime_s(&timeInfo, &now);
+    stringstream ss;
+
+    ss << "until_"
+        << std::setw(2) << std::setfill('0') << (timeInfo.tm_year + 1900) % 100
+        << std::setw(2) << std::setfill('0') << (timeInfo.tm_mon + 1)
+        << std::setw(2) << std::setfill('0') << timeInfo.tm_mday << "_"
+        << std::setw(2) << std::setfill('0') << timeInfo.tm_hour << "h_"
+        << std::setw(2) << std::setfill('0') << timeInfo.tm_min  << "m_"
+        << std::setw(2) << std::setfill('0') << timeInfo.tm_sec  << "s.log";
+    return ss.str();
+}
+
+bool Logger::fileSizeExceedsLimit(const string& fileName) {
+    ifstream file(fileName, ios::binary | ios::ate);
+    return file.is_open() && file.tellg() > MAX_FILE_SIZE;
+}
+
+void Logger::print(const string& classFuncName, const string& message) {
+    openLogFile();
 
     if (logFile.is_open()) {
         time_t now = time(nullptr);
@@ -29,14 +74,14 @@ void Logger::print(const string& classFuncName, const string& message) {
 
         string fullFunctionName = classStr + "." + functionStr + "()";
 
-        cout << "[" << timeStr << "] "
-            << std::setw(30) << std::left << fullFunctionName 
-            << ": " << message << endl;
+        /*cout << "[" << timeStr << "] "
+            << std::setw(30) << std::left << fullFunctionName
+            << ": " << message << endl;*/
 
         logFile << "[" << timeStr << "] "
             << std::setw(30) << std::left << fullFunctionName
             << ": " << message << endl;
 
-        logFile.close();
+        closeLogFile();
     }
 }
--- SSD/logger.h
+++ SSD/logger.h
@@ -5,6 +5,7 @@
 #include <ctime>
 #include <sstream>
 #include <iomanip>
+#include <filesystem>
 
 using namespace std;
 
@@ -12,9 +13,15 @@ using namespace std;
 
 class Logger {
 public:
-    static void print(const string& classFuncName, const string& message); 
+    static void print(const string& classFuncName, const string& message);
+    static void openLogFile();
+    static bool fileSizeExceedsLimit(const string& fileName);
+    static string generateNewFileName();
+    static void renameOldLogFile();
+    static void closeLogFile();
 
 private:
     static ofstream logFile;
-    //const string logFileName = "latest.txt";
+    static const string logFileName;   // 로그 파일 이름
+    static const size_t MAX_FILE_SIZE; // 파일 최대 크기 (10 KB)
 };
