a5e1735e7e9e48519900e7c7a87d478cb9209975
yj410.lee <yongjae410@gmail.com>
2025-04-04 11:08:46 +0900

    flush function unit test

--- SSD/SSD_test.cpp
+++ SSD/SSD_test.cpp
@@ -502,3 +502,34 @@ TEST_F(SSDFixture, CommandBufferTest_ReadFromCommandBuffer2)
 	outputFile.close();
 	EXPECT_EQ(data, "0x00000000");
 }
+
+TEST_F(SSDFixture, FlushTest)
+{
+	bool ret = mySsd.run("W 0 0xAAAAAAAA");
+	EXPECT_EQ(ret, true);
+	ret = mySsd.run("W 1 0xAAAAAAAA");
+	EXPECT_EQ(ret, true);
+	ret = mySsd.run("W 2 0xAAAAAAAA");
+	EXPECT_EQ(ret, true);
+	ret = mySsd.run("E 95 10");
+	EXPECT_EQ(ret, true);
+
+	EXPECT_EQ(std::filesystem::exists(nandFilePath), false);
+
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/W 0 0xAAAAAAAA"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/W 1 0xAAAAAAAA"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/W 2 0xAAAAAAAA"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/E 95 10"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/5_empty"), true);
+
+	ret = mySsd.run("F");
+	EXPECT_EQ(ret, true);
+
+	EXPECT_EQ(std::filesystem::exists(nandFilePath), true);
+
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/1_empty"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/2_empty"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/3_empty"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/4_empty"), true);
+	EXPECT_EQ(std::filesystem::exists(bufferDirPath + "/5_empty"), true);
+}
